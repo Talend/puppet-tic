0c7b328e893176300f206006a2202f096e41730c
Alexander Grigoriev <agrigoriev@talend.com>
2016-07-14 08:44:52 +0200

    manually setting up the service

--- manifests/frontend/service.pp
+++ manifests/frontend/service.pp
@@ -1,5 +1,14 @@
 class tic::frontend::service {
-  # The service resource is in the config.pp due to dependency cycles of with file resources in $base/conf
-  #
-}
 
+  service { 'tomcat-ipaas-srv':
+    ensure     => running,
+    enable => true,
+    hasstatus  => true,
+    hasrestart => false,
+    start      => 'su -s /bin/bash -c \'CATALINA_HOME=/opt/apache-tomcat CATALINA_BASE=/srv/tomcat/ipaas-srv /opt/apache-tomcat/bin/catalina.sh start\' tomcat',
+    stop       => 'su -s /bin/bash -c \'CATALINA_HOME=/opt/apache-tomcat CATALINA_BASE=/srv/tomcat/ipaas-srv /opt/apache-tomcat/bin/catalina.sh stop\' tomcat',
+    status     => 'ps aux | grep \'catalina.base=\' | grep -v grep',
+    provider   => 'base',
+  }
+
+}
