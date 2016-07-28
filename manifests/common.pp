ff1b9d5cb0b44607f77ef71114f0bf79998d25e9
Alexander Grigoriev <agrigoriev@talend.com>
2016-07-28 11:47:26 +0200

    styling

--- manifests/common.pp
+++ manifests/common.pp
@@ -1,8 +1,9 @@
-# common
 class tic::common {
+
   file {'/etc/profile.d/java_home.sh':
     content => "export JAVA_HOME=${tic::java_home}\nexport PATH=\$JAVA_HOME/bin:\$PATH\n"
   }
+
   if $::t_subenv == 'build' {
     file {'/etc/facter/facts.d/t_static_facts.txt':
       ensure  => present,
@@ -11,5 +12,5 @@ class tic::common {
       content => template('tic/etc/facter/facts.d/t_static_facts.txt.erb');
     }
   }
-}
 
+}
