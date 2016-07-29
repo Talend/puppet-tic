3e4ab15d65e32770a211cba84395eb9f6b5249db
Alexander Grigoriev <agrigoriev@talend.com>
2016-07-29 17:09:33 +0200

    directly specifing the tic role class

--- examples/frontend.pp
+++ examples/frontend.pp
@@ -1,19 +1,17 @@
-$ec2_userdata    = '{}'
-$ec2_instance_id = 'i-foo'
-$t_role          = 'frontend'
-$t_profile       = 'ipaas'
-
 packagecloud::repo { 'talend/ticdonotuse':
   type         => 'rpm',
   master_token => $::packagecloud_tic_donotuse_token
 } ->
-class { 'java':
+class { '::java':
+  distribution => 'jre',
+  package      => 'jre1.8.0_60',
+  version      => '1.8.0_60-fcs',
 } ->
-class { 'tic::globals':
-  role                    => 'frontend',
+class { '::tic::frontend':
+  java_home               => '/usr/java/jre1.8.0_60',
   java_xmx                => 1024,
   web_enable_test_context => false,
   web_use_ssl             => true,
-  cms_node                => 'testcmsnode'
-} ->
-class { 'tic': }
+  cms_node                => 'testcmsnode',
+  elasticache_address     => 'some_elasticache_address',
+}
