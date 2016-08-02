e9c146e05672c417a1bd8ffc27f68daca4c27b88
Alexander Grigoriev <agrigoriev@talend.com>
2016-08-02 09:56:00 +0200

    engine config changes

--- manifests/engine/config.pp
+++ manifests/engine/config.pp
@@ -1,12 +1,14 @@
 # config class for the rt_flow
 class tic::engine::config {
 
-  $config_dir = "${tic::karaf_base_path}/etc"
+  $config_dir = "${tic::engine::params::karaf_base_path}/etc"
 
-  $mvn_repositories = "${tic::nexus_url}/content/repositories/${tic::account_id}@id=${tic::account_id}.release, ${tic::nexus_url}/content/repositories/${tic::account_id}-snapshots@snapshots@id=${tic::account_id}.snapshot, file:/opt/talend/ipaas/rt-flow/data/kar@id=kar.repository@multi"
+  $mvn_repositories = "${tic::engine::params::nexus_urls}/content/repositories/${tic::engine::params::account_id}@id=${tic::engine::params::account_id}.release, ${tic::engine::params::nexus_urls}/content/repositories/${tic::engine::params::account_id}-snapshots@snapshots@id=${tic::engine::params::account_id}.snapshot, file:/opt/talend/ipaas/rt-flow/data/kar@id=kar.repository@multi"
 
   #Decrypt
-  $nexus_password = $tic::userdata_nexus_password_secret
+  $nexus_password = $tic::engine::params::userdata_nexus_password_secret
+  $nexus_urls     = $tic::engine::params::nexus_urls
+  $account_id     = $tic::engine::params::account_id
 
   file {
     '/opt/talend/ipaas/.m2':
@@ -21,263 +23,144 @@ class tic::engine::config {
       require => File['/opt/talend/ipaas/.m2']
   }
 
-  ini_setting {
-
-    'karaf_featuresBoot':
-      ensure  => present,
-      path    => "${config_dir}/org.apache.karaf.features.cfg",
-      section => '',
-      setting => 'featuresBoot',
-      value   => $tic::karaf_boot_features_real;
-
-    'account_id':
-      ensure  => present,
-      path    => "${config_dir}/org.talend.ipaas.rt.deployment.agent.cfg",
-      section => '',
-      setting => 'account.id',
-      value   => $tic::account_id;
-
-    'container_id':
-      ensure  => present,
-      path    => "${config_dir}/org.talend.ipaas.rt.deployment.agent.cfg",
-      section => '',
-      setting => 'container.id',
-      value   => $tic::container_id;
-
-    'container_id_heartbeat':
-      ensure  => present,
-      path    => "${config_dir}/org.talend.ipaas.rt.heartbeat.sender.cloud.cfg",
-      section => '',
-      setting => 'container.id',
-      value   => $tic::container_id;
-
-    'account_id_heartbeat':
-      ensure  => present,
-      path    => "${config_dir}/org.talend.ipaas.rt.heartbeat.sender.cloud.cfg",
-      section => '',
-      setting => 'account.id',
-      value   => $tic::account_id;
-
-    'heartbeat_interval':
-      ensure  => present,
-      path    => "${config_dir}/org.talend.ipaas.rt.heartbeat.sender.cloud.cfg",
-      section => '',
-      setting => 'heartbeat.interval',
-      value   => $tic::heartbeat_interval;
-
-    'queue_input_name':
-      ensure  => present,
-      path    => "${config_dir}/org.talend.ipaas.rt.deployment.agent.cfg",
-      section => '',
-      setting => 'queue.input.name',
-      value   => $tic::queue_input_name;
-
-    'queue_response_name':
-      ensure  => present,
-      path    => "${config_dir}/org.talend.ipaas.rt.deployment.agent.cfg",
-      section => '',
-      setting => 'queue.response.name',
-      value   => $tic::queue_response_name;
-
-    'queue_input_consumers_count':
-      ensure  => present,
-      path    => "${config_dir}/org.talend.ipaas.rt.deployment.agent.cfg",
-      section => '',
-      setting => 'queue.input.consumers.count',
-      value   => $tic::queue_input_consumers_count;
-
-    'mvn_repositories':
-      ensure  => present,
-      path    => "${config_dir}/org.ops4j.pax.url.mvn.cfg",
-      section => '',
-      setting => 'org.ops4j.pax.url.mvn.repositories',
-      value   => '';
-
-    'mvn_settings':
-      ensure  => present,
-      path    => "${config_dir}/org.ops4j.pax.url.mvn.cfg",
-      section => '',
-      setting => 'org.ops4j.pax.url.mvn.settings',
-      value   => '/opt/talend/ipaas/.m2/settings.xml';
-
-    'flow_undeploy_after_completed':
-      ensure  => present,
-      path    => "${config_dir}/org.talend.ipaas.rt.deployment.agent.cfg",
-      section => '',
-      setting => 'flow.undeploy.after.completed',
-      value   => $tic::flow_undeploy_after_completed;
-
-    'dispatcher_input_queue':
-      ensure  => present,
-      path    => "${config_dir}/org.talend.ipaas.rt.dispatcher.client.cfg",
-      section => '',
-      setting => 'dispatcher.input.queue',
-      value   => $tic::dispatcher_input_queue;
-
-    'dispatcher_response_queue':
-      ensure  => present,
-      path    => "${config_dir}/org.talend.ipaas.rt.dispatcher.client.cfg",
-      section => '',
-      setting => 'dispatcher.response.queue',
-      value   => $tic::dispatcher_response_queue;
-
-    'activemq_broker_url':
-      ensure  => present,
-      path    => "${config_dir}/org.talend.ipaas.rt.eventsource.amq.cfg",
-      section => '',
-      setting => 'activemq.broker.url',
-      value   => $tic::activemq_broker_url;
-
-    'activemq_broker_username':
-      ensure  => present,
-      path    => "${config_dir}/org.talend.ipaas.rt.eventsource.amq.cfg",
-      section => '',
-      setting => 'activemq.broker.username',
-      value   => $tic::activemq_broker_username;
-
-    'activemq_broker_password':
-      ensure  => present,
-      path    => "${config_dir}/org.talend.ipaas.rt.eventsource.amq.cfg",
-      section => '',
-      setting => 'activemq.broker.password',
-      value   => $tic::activemq_broker_password;
-
-
-    'eventlogging_jms_url':
-      ensure  => present,
-      path    => "${config_dir}/org.talend.eventlogging.sender.jms.cfg",
-      section => '',
-      setting => 'sender.destination.jms.url',
-      value   => $tic::activemq_broker_url;
-
-    'dts_service_url':
-      ensure  => present,
-      path    => "${config_dir}/org.talend.ipaas.rt.dts.client.cfg",
-      section => '',
-      setting => 'dts.service.url',
-      value   => $tic::dts_service_url;
-
-    'eventlogging_destination':
-      ensure  => present,
-      path    => "${config_dir}/org.talend.eventlogging.agent.cfg",
-      section => '',
-      setting => 'agent.sender.destination.default',
-      value   => 'eventlogsenderjms';
-
-    'agent_id':
-      ensure  => present,
-      path    => "${config_dir}/org.talend.eventlogging.agent.cfg",
-      section => '',
-      setting => 'agentid',
-      value   => $tic::account_id;
-
-    'custom_info_container_id':
-      ensure  => present,
-      path    => "${config_dir}/org.talend.eventlogging.agent.cfg",
-      section => '',
-      setting => 'event.add.customInfo.containerID',
-      value   => $tic::container_id;
-
-    'custom_info_account_id':
-      ensure  => present,
-      path    => "${config_dir}/org.talend.eventlogging.agent.cfg",
-      section => '',
-      setting => 'event.add.customInfo.accountID',
-      value   => $tic::account_id;
-
-    'debug_logging_flow':
-      ensure  => present,
-      path    => "${config_dir}/org.ops4j.pax.logging.cfg",
-      section => '',
-      setting => 'log4j.logger.org.talend.ipaas',
-      value   => $tic::logging_level;
-
-    'log_amq_messages_flow':
-      ensure  => present,
-      path    => "${config_dir}/org.talend.ipaas.rt.eventsource.amq.cfg",
-      section => '',
-      setting => 'log.messages',
-      value   => $tic::log_amq_messages;
-  }
-
-  if $tic::karaf_rmiRegistryPort {
-    ini_setting { 'karaf_rmiRegistryPort':
-      ensure  => present,
-      path    => "${config_dir}/org.apache.karaf.management.cfg",
-      section => '',
-      setting => 'rmiRegistryPort',
-      value   => $tic::karaf_rmiRegistryPort;
+  tic::ini_settings { "${config_dir}/org.apache.karaf.features.cfg":
+    settings => {
+      'featuresBoot' => $tic::engine::params::karaf_boot_features_real,
     }
   }
 
-  if $tic::karaf_rmiServerPort {
-    ini_setting { 'karaf_rmiServerPort':
-      ensure  => present,
-      path    => "${config_dir}/org.apache.karaf.management.cfg",
-      section => '',
-      setting => 'rmiServerPort',
-      value   => $tic::karaf_rmiServerPort;
+  tic::ini_settings { "${config_dir}/org.talend.ipaas.rt.deployment.agent.cfg":
+    settings => {
+      'account.id'                    => $tic::engine::params::account_id,
+      'container.id'                  => $tic::engine::params::container_id,
+      'queue.input.name'              => $tic::engine::params::queue_input_name,
+      'queue.response.name'           => $tic::engine::params::queue_response_name,
+      'queue.input.consumers.count'   => $tic::engine::params::queue_input_consumers_count,
+      'flow.undeploy.after.completed' => $tic::engine::params::flow_undeploy_after_completed,
     }
   }
 
-  if $tic::karaf_httpPort {
-    ini_setting { 'karaf_httpPort':
-      ensure  => present,
-      path    => "${config_dir}/org.ops4j.pax.web.cfg",
-      section => '',
-      setting => 'org.osgi.service.http.port',
-      value   => $tic::karaf_httpPort;
+  tic::ini_settings { "${config_dir}/org.talend.ipaas.rt.heartbeat.sender.cloud.cfg":
+    settings => {
+      'container.id'       => $tic::engine::params::container_id,
+      'account.id'         => $tic::engine::params::account_id,
+      'heartbeat.interval' => $tic::engine::params::heartbeat_interval,
     }
   }
 
-  if $tic::karaf_httpsPort {
-    ini_setting { 'karaf_httpsPort':
-      ensure  => present,
-      path    => "${config_dir}/org.ops4j.pax.web.cfg",
-      section => '',
-      setting => 'org.osgi.service.http.port.secure',
-      value   => $tic::karaf_httpsPort;
+  tic::ini_settings { "${config_dir}/org.ops4j.pax.url.mvn.cfg":
+    settings => {
+      'org.ops4j.pax.url.mvn.repositories' => '',
+      'org.ops4j.pax.url.mvn.settings'     => '/opt/talend/ipaas/.m2/settings.xml',
     }
   }
 
-  if $tic::karaf_sshPort {
-    ini_setting { 'karaf_sshPort':
-      ensure  => present,
-      path    => "${config_dir}/org.apache.karaf.shell.cfg",
-      section => '',
-      setting => 'sshPort',
-      value   => $tic::karaf_sshPort;
+  tic::ini_settings { "${config_dir}/org.talend.ipaas.rt.dispatcher.client.cfg":
+    settings => {
+      'dispatcher.input.queue'    => $tic::engine::params::dispatcher_input_queue,
+      'dispatcher.response.queue' => $tic::engine::params::dispatcher_response_queue,
     }
   }
 
-  if $tic::karaf_command_server_port {
-    ini_setting { 'karaf_command_server_port':
-      ensure  => present,
-      path    => "${config_dir}/org.talend.remote.jobserver.server.cfg",
-      section => '',
-      setting => 'org.talend.remote.jobserver.server.TalendJobServer.COMMAND_SERVER_PORT',
-      value   => $tic::karaf_command_server_port;
+  tic::ini_settings { "${config_dir}/org.talend.ipaas.rt.eventsource.amq.cfg":
+    settings => {
+      'activemq.broker.url'      => $tic::engine::params::activemq_broker_url,
+      'activemq.broker.username' => $tic::engine::params::activemq_broker_username,
+      'activemq.broker.password' => $tic::engine::params::activemq_broker_password,
+      'log.messages'             => $tic::engine::params::log_amq_messages,
     }
   }
 
-  if $tic::karaf_file_server_port {
-    ini_setting { 'karaf_file_server_port':
-      ensure  => present,
-      path    => "${config_dir}/org.talend.remote.jobserver.server.cfg",
-      section => '',
-      setting => 'org.talend.remote.jobserver.server.TalendJobServer.FILE_SERVER_PORT',
-      value   => $tic::karaf_file_server_port;
+  tic::ini_settings { "${config_dir}/org.talend.eventlogging.sender.jms.cfg":
+    settings => {
+      'sender.destination.jms.url' => $tic::engine::params::activemq_broker_url,
     }
   }
 
-  if $tic::karaf_monitoring_port {
-    ini_setting { 'karaf_monitoring_port':
-      ensure  => present,
-      path    => "${config_dir}/org.talend.remote.jobserver.server.cfg",
-      section => '',
-      setting => 'org.talend.remote.jobserver.server.TalendJobServer.MONITORING_PORT',
-      value   => $tic::karaf_monitoring_port;
+  tic::ini_settings { "${config_dir}/org.talend.ipaas.rt.dts.client.cfg":
+    settings => {
+      'dts.service.url' => $tic::engine::params::dts_service_url,
     }
   }
+
+  tic::ini_settings { "${config_dir}/org.talend.eventlogging.agent.cfg":
+    settings => {
+      'agent.sender.destination.default' => 'eventlogsenderjms',
+      'agentid'                          => $tic::engine::params::account_id,
+      'event.add.customInfo.containerID' => $tic::engine::params::container_id,
+      'event.add.customInfo.accountID'   => $tic::engine::params::account_id,
+    }
+  }
+
+  tic::ini_settings { "${config_dir}/org.ops4j.pax.logging.cfg":
+    settings => {
+      'log4j.logger.org.talend.ipaas' => $tic::engine::params::logging_level,
+    }
+  }
+
+
+  if $tic::engine::params::karaf_rmiRegistryPort {
+    tic::ini_settings { "${config_dir}/org.apache.karaf.management.cfg":
+      settings => {
+        'rmiRegistryPort' => $tic::engine::params::karaf_rmiRegistryPort,
+      }
+    }
+  }
+
+  if $tic::engine::params::karaf_rmiServerPort {
+    tic::ini_settings { "${config_dir}/org.apache.karaf.management.cfg":
+      settings => {
+        'rmiServerPort' => $tic::engine::params::karaf_rmiServerPort,
+      }
+    }
+  }
+
+  if $tic::engine::params::karaf_httpPort {
+    tic::ini_settings { "${config_dir}/org.ops4j.pax.web.cfg":
+      settings => {
+        'org.osgi.service.http.port' => $tic::engine::params::karaf_httpPort,
+      }
+    }
+  }
+
+  if $tic::engine::params::karaf_httpsPort {
+    tic::ini_settings { "${config_dir}/org.ops4j.pax.web.cfg":
+      settings => {
+        'org.osgi.service.http.port.secure' => $tic::engine::params::karaf_httpsPort,
+      }
+    }
+  }
+
+  if $tic::engine::params::karaf_sshPort {
+    tic::ini_settings { "${config_dir}/org.apache.karaf.shell.cfg":
+      settings => {
+        'sshPort' => $tic::engine::params::karaf_sshPort,
+      }
+    }
+  }
+
+  if $tic::engine::params::karaf_command_server_port {
+    tic::ini_settings { "${config_dir}/org.talend.remote.jobserver.server.cfg":
+      settings => {
+        'org.talend.remote.jobserver.server.TalendJobServer.COMMAND_SERVER_PORT' => $tic::engine::params::karaf_command_server_port,
+      }
+    }
+  }
+
+  if $tic::engine::params::karaf_file_server_port {
+    tic::ini_settings { "${config_dir}/org.talend.remote.jobserver.server.cfg":
+      settings => {
+        'org.talend.remote.jobserver.server.TalendJobServer.FILE_SERVER_PORT' => $tic::engine::params::karaf_file_server_port,
+      }
+    }
+  }
+
+  if $tic::engine::params::karaf_monitoring_port {
+    tic::ini_settings { "${config_dir}/org.talend.remote.jobserver.server.cfg":
+      settings => {
+        'org.talend.remote.jobserver.server.TalendJobServer.MONITORING_PORT' => $tic::engine::params::karaf_monitoring_port,
+      }
+    }
+  }
+
 }
-
