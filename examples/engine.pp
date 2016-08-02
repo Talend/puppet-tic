022b781239ff77b7a9cb653047a89e5e055e12cb
Alexander Grigoriev <agrigoriev@talend.com>
2016-08-02 11:11:46 +0200

    adjusting tic::engine test script

--- examples/engine.pp
+++ examples/engine.pp
@@ -1,9 +1,3 @@
-$ec2_userdata    = '{}'
-$ec2_instance_id = 'i-foo'
-$t_role          = 'engine'
-$t_profile       = 'ipaas'
-$t_subenv        = 'build'
-
 packagecloud::repo { 'talend/ticdonotuse':
   type         => 'rpm',
   master_token => $::packagecloud_tic_donotuse_token
@@ -13,31 +7,7 @@ class { '::java':
   package      => 'jre1.8.0_60',
   version      => '1.8.0_60-fcs',
 } ->
-file {
-  '/etc/facter':
-    ensure => directory;
-  '/etc/facter/facts.d':
-    ensure => directory;
-} ->
-file { '/etc/sysconfig/clock':
-  ensure  => present,
-  content => "
-UTC=true
-ZONE=UTC
-"
-} ->
-package { 'epel-release':
-  ensure => installed,
-} ->
-class { 'tic::globals':
-  role               => 'engine',
+class { '::tic::engine':
+  java_home          => '/usr/java/jre1.8.0_60',
   rt_flow_lxc_enable => true,
-  java_home          => '/usr/java/jre1.8.0_60/',
-  java_xmx           => 1024,
-} ->
-class { 'tic':
-  jmx_enabled => true,
-} ->
-exec { 'start rt-flow-service-lxc':
-  command => '/usr/sbin/service rt-flow-service-lxc start',
 }
