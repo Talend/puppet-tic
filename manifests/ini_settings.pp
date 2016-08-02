e3051ffe5781cc31a6e8ab3b45ceffa993eb6db0
Alexander Grigoriev <agrigoriev@talend.com>
2016-08-02 11:14:14 +0200

    ini_settings resource updated

--- manifests/ini_settings.pp
+++ manifests/ini_settings.pp
@@ -7,19 +7,23 @@ define tic::ini_settings (
   $glue     = '{{{'
 
 ) {
-  validate_string($path)
+
+  $real_path = pick($path, $name)
+
+  validate_string($real_path)
   validate_hash($settings)
   validate_string($section)
   validate_string($glue)
 
-  $_settings = prefix($settings, "${path}:")
+  $_settings = prefix($settings, "${real_path}:")
 
   $joined = join_keys_to_values( $_settings, $glue )
 
   tic::ini_setting_wrapper { $joined:
     ensure  => $ensure,
     glue    => $glue,
-    path    => $path,
+    path    => $real_path,
     section => $section,
   }
+
 }
