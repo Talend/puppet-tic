2c4ae4e68df18124b940fc4357b8891a424b36d4
Alexander Grigoriev <agrigoriev@talend.com>
2016-08-03 10:05:31 +0200

    respecting lint rules

--- examples/services.pp
+++ examples/services.pp
@@ -11,7 +11,7 @@ class { '::tic::services':
   java_home                => '/usr/java/jre1.8.0_60',
   activemq_broker_username => 'activemq_broker_test_username',
   activemq_broker_password => 'activemq_broker_test_password',
-  pe_service_username => 'pe_service_test_username',
+  pe_service_username      => 'pe_service_test_username',
   karaf_features_install   => [
     'plan_executor_client',
     'plan_executor_client',
