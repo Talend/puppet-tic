8aac3fc04de4ee5f563b6346d3704879fe6de924
Alexander Grigoriev <agrigoriev@talend.com>
2016-12-05 14:26:10 +0100

    specs updated

--- examples/services.pp
+++ examples/services.pp
@@ -19,6 +19,7 @@ class { '::tic::services':
   rt_flow_t_environment    => 'test_env',
   karaf_service_ensure     => 'running',
   karaf_service_enable     => true,
+  osgi_http_service_port   => 8282,
   karaf_features_install   => [
     'plan_executor_client',
     'plan_executor_client',
