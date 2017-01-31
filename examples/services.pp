b34e35d7f651b0ff2497f0e2b01a610f6f63612b
Alexander Grigoriev <agrigoriev@talend.com>
2017-01-31 09:53:29 +0100

    typo

--- examples/services.pp
+++ examples/services.pp
@@ -33,8 +33,7 @@ class { '::tic::services':
   ],
 }
 
-
-if versioncmp($ipaas_rt_infra_installed_version, '1.5') or versioncmp($ipaas_rt_infra_build_version, '1.5') {
+if versioncmp($ipaas_rt_infra_installed_version, '1.5') > 0 or versioncmp($ipaas_rt_infra_build_version, '1.5') > 0 {
   class {
     'tic::services20':
       karaf_features_install => ['tipaas-account-manager-service'],
