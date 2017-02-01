3c96d0d513b073fa07153cbc417f14a2e3bbf283
Alexander Grigoriev <agrigoriev@talend.com>
2017-02-01 09:22:48 +0100

    code style

--- manifests/services20.pp
+++ manifests/services20.pp
@@ -1,7 +1,9 @@
-class tic::services20(
+class tic::services20 (
+
   $karaf_features_install = [],
-  $iam_service_node = undef,
-  $scim_service_node = undef
+  $iam_service_node       = undef,
+  $scim_service_node      = undef,
+
 ) {
 
   contain ::tic::services20::config
