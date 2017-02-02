924d72b186cd2955fba2abe4799f5d739422d850
Stefan Baryakov <sbaryakov@talend.com>
2017-02-02 17:33:18 +0100

    add scim url to services config

--- manifests/frontend20/config.pp
+++ manifests/frontend20/config.pp
@@ -2,10 +2,12 @@ class tic::frontend20::config {
 
   require ::tic::frontend::install
 
-  tic::ini_settings { 'scim_service_url':
-    path     => '/srv/tomcat/ipaas-srv/webapps/ipaas-server/WEB-INF/classes/ipaas_server.properties',
-    settings => {
-      'scim_service_url'   => "http://${tic::frontend20::scim_service_node}"
-    }
+  tic::ini_settings {
+    'scim_service_url_ipaas_server':
+      path     => '/srv/tomcat/ipaas-srv/webapps/ipaas-server/WEB-INF/classes/ipaas_server.properties',
+      settings => { 'scim_service_url'   => "http://${tic::frontend20::scim_service_node}" };
+    'scim_service_url_inventory':
+      path     => '/srv/tomcat/ipaas-srv/webapps/ipaas-services/WEB-INF/classes/config.properties',
+      settings => { 'scim_service_url'   => "http://${tic::frontend20::scim_service_node}" };
   }
 }
