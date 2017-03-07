4c8c22eec67cceb1272f359ee0accb512cd45664
Alexander Grigoriev <agrigoriev@talend.com>
2017-03-07 09:31:46 +0100

    resource renamed

--- manifests/frontend20/config.pp
+++ manifests/frontend20/config.pp
@@ -2,7 +2,7 @@ class tic::frontend20::config {
 
   require ::tic::frontend::install
 
-  tic::ini_settings { 'scim_service_url':
+  tic::ini_settings { 'ipaas_scim_service_url':
     path     => '/srv/tomcat/ipaas-srv/webapps/ipaas-server/WEB-INF/classes/ipaas_server.properties',
     settings => {
       'scim_service_url'   => "http://${tic::frontend20::scim_service_node}"
