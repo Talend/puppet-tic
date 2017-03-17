fdf575cf47bf18c9a155ea4591be1484fc572ad6
Alexander Grigoriev <agrigoriev@talend.com>
2017-03-17 10:38:16 +0100

    respecting lint rules

--- manifests/frontend20/params.pp
+++ manifests/frontend20/params.pp
@@ -1,6 +1,5 @@
 class tic::frontend20::params {
 
-  
   $scim_service_node = pick($tic::frontend20::scim_service_node, 'localhost')
   $scim_service_url  = pick($tic::frontend20::scim_service_url, "http://${scim_service_node}")
 
