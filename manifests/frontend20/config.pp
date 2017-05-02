8f8745be844c8a1a12759d64dc3325c43eccf7e1
Stefan Baryakov <sbaryakov@talend.com>
2017-05-02 14:19:54 +0200

    formating

--- manifests/frontend20/config.pp
+++ manifests/frontend20/config.pp
@@ -2,25 +2,23 @@ class tic::frontend20::config {
 
   require ::tic::frontend::install
 
-  tic::ini_settings { 'ipaas_scim_service_url':
-    path     => '/srv/tomcat/ipaas-srv/webapps/ipaas-server/WEB-INF/classes/ipaas_server.properties',
-    settings => {
-      'scim_service_url'   => $tic::frontend20::params::scim_service_url,
-    }
-  }
+  tic::ini_settings {
+    'ipaas_webapp_server_settings':
+      path     => '/srv/tomcat/ipaas-srv/webapps/ipaas-server/WEB-INF/classes/ipaas_server.properties',
+      settings => {
+        'scim_service_url'         => $tic::frontend20::params::scim_service_url,
+      };
 
-  tic::ini_settings { 'ipaas_api_scim_service_url':
-    path     => '/srv/tomcat/ipaas-srv/webapps/api/WEB-INF/classes/ipaas_api.properties',
-    settings => {
-      'scim_service_url'   => $tic::frontend20::params::scim_service_url,
-    }
-  }
-
-  tic::ini_settings { 'ipaas_services_scim_service_url':
-    path     => '/srv/tomcat/ipaas-srv/webapps/ipaas-services/WEB-INF/classes/config.properties',
-    settings => {
-      'scim_service_url'   => $tic::frontend20::params::scim_service_url,
-    }
+    'ipaas_api_scim_service_url':
+      path     => '/srv/tomcat/ipaas-srv/webapps/api/WEB-INF/classes/ipaas_api.properties',
+      settings => {
+        'scim_service_url'   => $tic::frontend20::params::scim_service_url,
+      };
+    'ipaas_services_scim_service_url':
+      path     => '/srv/tomcat/ipaas-srv/webapps/ipaas-services/WEB-INF/classes/config.properties',
+      settings => {
+        'scim_service_url'   => $tic::frontend20::params::scim_service_url,
+      };
   }
 
   file_line { 'client application clientId':
