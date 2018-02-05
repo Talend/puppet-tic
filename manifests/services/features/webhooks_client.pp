ece98a7ac0baf167fdbb2e1a3952ff2ded11886c
Igor Lazebny <ilazebny@talend.com>
2018-02-05 11:27:17 +0200

    DEVOPS-3766: Add webhook client config (#22)

---
+++ manifests/services/features/webhooks_client.pp
@@ -0,0 +1,11 @@
+class tic::services::features::webhooks_client {
+
+  $config_dir = "${tic::services::params::karaf_base_path}/etc"
+
+  tic::ini_settings { "${config_dir}/org.talend.ipaas.rt.webhooks.client.cfg":
+    settings => {
+      'webhooks.service.url' => $tic::services::params::webhooks_external_url,
+    }
+  }
+
+}
