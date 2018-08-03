4a319e7c28e9d6880d75b9a7b7669fbca9997d7b
Igor Lazebny <ilazebny@talend.com>
2018-08-03 17:48:52 +0300

    DEVOPS-5034: Remove test data bucket

--- manifests/services/features/data_transfer_service_core.pp
+++ manifests/services/features/data_transfer_service_core.pp
@@ -5,7 +5,6 @@ class tic::services::features::data_transfer_service_core {
   tic::ini_settings { "${config_dir}/org.talend.ipaas.rt.dts.core.cfg":
     settings => {
       's3.bucket.name.rejected.data' => $tic::services::params::dts_s3_bucket_rejected_data,
-      's3.bucket.name.test.data'     => $tic::services::params::dts_s3_bucket_test_data,
       's3.bucket.name.log.data'      => $tic::services::params::dts_s3_bucket_logs_data,
       's3.bucket.name.download'      => $tic::services::params::dts_s3_bucket_downloads_data,
       'max.data.size'                => $tic::services::params::dts_max_data_size,
