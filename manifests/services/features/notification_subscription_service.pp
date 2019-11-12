a259fc541a0630508093c9c299d6c4e6bffa94bb
Mykhailo Sevastian <msevastian@talend.com>
2019-11-12 14:27:19 +0200

    check4

--- manifests/services/features/notification_subscription_service.pp
+++ manifests/services/features/notification_subscription_service.pp
@@ -4,15 +4,15 @@ class tic::services::features::notification_subscription_service {
 
   tic::ini_settings { "${config_dir}/org.talend.ipaas.rt.notification.subscription.service.cfg":
     settings => {
-      'db.host'                    => $tic::services::params::notification_subscription_db_host,
-      'db.port'                    => $tic::services::params::notification_subscription_db_port,
-      'db.name'                    => $tic::services::params::notification_subscription_db_name,
-      'db.username'                => $tic::services::params::notification_subscription_db_username,
-      'db.password'                => $tic::services::params::notification_subscription_db_password,
-      'db.max_connections'         => $tic::services::params::notification_subscription_db_max_connections,
-      'db.redis.host'              => $tic::services::params::redis_cache_host,
-      'db.redis.port'              => $tic::services::params::redis_cache_port,
-      'db.redis.database'          => $tic::services::params::redis_cache_db_tmc,
+      'db.host'             => $tic::services::params::notification_subscription_db_host,
+      'db.port'             => $tic::services::params::notification_subscription_db_port,
+      'db.name'             => $tic::services::params::notification_subscription_db_name,
+      'db.username'         => $tic::services::params::notification_subscription_db_username,
+      'db.password'         => $tic::services::params::notification_subscription_db_password,
+      'db.max_connections'  => $tic::services::params::notification_subscription_db_max_connections,
+      'db.redis.host'       => $tic::services::params::redis_cache_host,
+      'db.redis.port'       => $tic::services::params::redis_cache_port,
+      'db.redis.database'   => $tic::services::params::redis_cache_db_tmc,
     }
   }
 
