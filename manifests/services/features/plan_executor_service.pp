8af7c42147bf6073e36e40b31525bd552652245d
Mykhailo Sevastian <msevastian@talend.com>
2020-06-25 14:29:17 +0300

    add redis connection to plan executor service

--- manifests/services/features/plan_executor_service.pp
+++ manifests/services/features/plan_executor_service.pp
@@ -1,11 +1,19 @@
 class tic::services::features::plan_executor_service {
 
+  $redis_host = $tic::services::params::dispatcher_redis_host # using same redis host as for dispatcher [DEVOPS-10131]
+
   $config_dir = "${tic::services::params::karaf_base_path}/etc"
 
   tic::ini_settings { "${config_dir}/org.talend.ipaas.rt.pe.datasource.cfg":
     settings => {
       'datasource.servername' => $tic::services::params::pe_db_host,
       'datasource.password'   => $tic::services::params::pe_db_password,
+    }
+  }
+
+  tic::ini_settings { "${config_dir}/org.talend.ipaas.rt.pe.service.cfg":
+    settings => {
+      'db.host' => $redis_host
     }
   }
 
